b8e2e1dbc82e254a6fc748d5c76200e22af4758c
thanson47 <thanson042@gmail.com>
2025-01-25 22:44:01 -0500

    pre pysol

--- Helmholtz Driver V2/Helmholtz Driver/main.py
+++ Helmholtz Driver V2/Helmholtz Driver/main.py
@@ -1,25 +1,14 @@
 # Adafruit Mag code
 
-import bleak
-import asyncio
-import struct
 import time
-import numpy
-import serial
 import math
-
 import matplotlib.pyplot as plt
 
 from R4UART import sendPWMValues, readPWMValues, initiateUART, readMagnetometerValues # UART code 
-#from PID import PIDsetpoints, computePID # PID code
-from PID_new import xPID, yPID, zPID
+from PID import xPID, yPID, zPID # PID code
 from calibrateValues import calibrate # magnetometer calibration code 
 from extraneous import processStrings, calculateOffsets # import extraneous functions
 
-debug = False # enables extra print statements (slow)
-manual = False # when false, PID is enabled
-initializing = False # leave true to find earth magnetic field offsets for PID
-numOfOffsetVals = 50 # number of measurements script will take before calculating earth offset 
 xOffset = 0
 yOffset = 0
 zOffset = 0
@@ -30,7 +19,6 @@ terminals = initiateUART()
 time.sleep(1)
 nanoSer = terminals[0]
 R4Ser = terminals[1]
-
 
 # initial duty cycles, for manual mode set desired ones here
 Xp = 0.0
@@ -52,14 +40,10 @@ xRamp = 0
 yRamp = 0
 zRamp = 0
 
-    # arrays to hold initial offset values
-xAvg = []
-yAvg = []
-zAvg = []
 
 maxVal = 100 # max value of pwm signal (control output)
 
-    # turn off cage at start
+# turn off cage at start
 sendPWMValues(0, 0, 0, 0, 0, 0, R4Ser)
 time.sleep(2)
 
@@ -80,7 +64,7 @@ timeVec = [0]
 
 while (time.time()-startTime < 15):
     timeVec.append(time.time()-startTime)               
-    ################################################################################################################## magnetometer reading
+   
     #Change set values
 #     if (time.time()-startTime < 4):
 #         set_index = 0
@@ -100,6 +84,8 @@ while (time.time()-startTime < 15):
     zRamp = 30 * math.cos(6 * math.pi * (time.time()-startTime) / 15)
     
     #print("X: " + "{:.2f}".format(magX) + " Y: " + "{:.2f}".format(magY) + " Z: " + "{:.2f}".format(magZ))
+
+    ################################################################################################################## magnetometer reading
     nanoSer.reset_input_buffer()
     nanoSer.reset_output_buffer()
     
@@ -130,15 +116,6 @@ while (time.time()-startTime < 15):
     print("X: " + str(magStrings[0]) + " Y: " + str(magStrings[1]) + " Z: " + str(magStrings[2]))
     ##################################################################################################################
 
-    if(initializing):
-        xAvg.append(calMagX)
-        yAvg.append(magY)
-        zAvg.append(magZ)
-        if(len(xAvg) >= numOfOffsetVals):
-            xOffset, yOffset, zOffset = calculateOffsets(xAvg, yAvg, zAvg)
-            initializing = False
-            #print(str(xOffset) + " " + str(yOffset) + " " + str(zOffset))
-
     [Xp, Xn] = xPID(xRamp, calMagX, magOutputX[i-1], pwmPosOutputX[i-1], pwmNegOutputX[i-1], maxVal, timeVec[i]-timeVec[i-1])
     [Yp, Yn] = yPID(yRamp, calMagY, magOutputY[i-1], pwmPosOutputY[i-1], pwmNegOutputY[i-1], maxVal, timeVec[i]-timeVec[i-1])
     [Zp, Zn] = zPID(zRamp, calMagZ, magOutputZ[i-1], pwmPosOutputZ[i-1], pwmNegOutputZ[i-1], maxVal, timeVec[i]-timeVec[i-1]) 
@@ -153,16 +130,10 @@ while (time.time()-startTime < 15):
     pwmPosOutputZ.append(Zp)
     pwmNegOutputZ.append(Zn)
 
-        
-        #print(str(xTemp) + " " + str(yTemp) + " " + str(zTemp))
 
     time.sleep(0.1)
-    #sendPWMValues(Yp, Yn, 0, 0, 0, 0, R4Ser)
-    #sendPWMValues(0, 0, Xn, Xp, 0, 0, R4Ser)
-    #sendPWMValues(0, 0, 0, 0, Zp, Zn, R4Ser)
+
     sendPWMValues(Yp, Yn, Xn, Xp, Zp, Zn, R4Ser)
-    #readPWMValues(R4Ser)
-    #sendPWMValues(Yp, Yn, Xn, Xp, Zp, Zn, R4Ser) # sends PWM to R4 (currently trying with 1 direction)
 
     i+=1
     
---
+++ test folder/testfunction.py
@@ -0,0 +1,4 @@
+
+
+def tester():
+    print("HELLOW")
