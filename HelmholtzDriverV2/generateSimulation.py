c926ce8c9ad5f6dc611500ccead829ad1c02c463
thanson47 <thanson042@gmail.com>
2025-02-06 17:15:01 -0500

    run sim

--- HelmholtzDriverV2/generateSimulation.py
+++ HelmholtzDriverV2/generateSimulation.py
@@ -15,7 +15,7 @@ from Dependencies.extraneous import processStrings, calculateOffsets, millis # i
 
 ########################################################################################## Settings
 
-runValues = 50 # number of values of magnetic fields to loop through, they are in increments of seconds so 100 is 100 seconds of the sim
+runValues = 120 # number of values of magnetic fields to loop through, they are in increments of seconds so 100 is 100 seconds of the sim
 startPos = 5060 # starting position (in time) of the pysol simulation, so 0 seconds is at the begining 
 runSpeed = 1 # percentage of how fast simulation should be processed, 1 is 100% real time, 0.1 is 10x faster
 renderFidelity = 10
@@ -93,9 +93,6 @@ timeVector = [0]
 simPos = startPos + 1 # simulation position
 i = 1 # array positions
 
-i_best = 1 # best attempt index
-err_best = 10000 # best attempt error
-
 print("Running")
 
 appendedTimes = 0
@@ -165,24 +162,15 @@ while (simPos < len(dataFrame)):
     simulationProgressX.append(currentFields[0])
     simulationProgressY.append(currentFields[1])
     simulationProgressZ.append(currentFields[2])
-
-    err_current = (abs(simulationProgressX[i] - magOutputX[i])) + (abs(simulationProgressY[i] - magOutputY[i])) + (abs(simulationProgressZ[i] - magOutputZ[i]))
-    print("Err Current: ", end=" ")
-    print(err_current)
-    if (err_current < err_best):
-        print("Err Best: ", end=" ")
-        print(err_best)
-        err_best = err_current
-        i_best = i
     
     # Doesn't append anything until set time has elapsed
     appendedTimes += 1
     if((millis() - t0) > (1000 * runSpeed) + (renderFidelity * 13)):
         # Adds relevant info to dataframe for output csv
-        row = pd.DataFrame([{"SIM X": simulationProgressX[i_best], "SIM Y": simulationProgressY[i_best], "SIM Z": simulationProgressZ[i_best], 
-                             "PWM_X+": pwmPosOutputX[i_best], "PWM_X-": pwmNegOutputX[i_best],
-                             "PWM_Y+": pwmPosOutputY[i_best], "PWM_Y-": pwmNegOutputY[i_best],
-                             "PWM_Z+": pwmPosOutputZ[i_best], "PWM_Z-": pwmNegOutputZ[i_best],}])
+        row = pd.DataFrame([{"SIMX": currentFields[0], "SIMY": currentFields[1], "SIMZ": currentFields[0], 
+                             "PWM_X+": Xp, "PWM_X-": Xn,
+                             "PWM_Y+": Yp, "PWM_Y-": Yn,
+                             "PWM_Z+": Zp, "PWM_Z-": Zn,}])
         
         df = pd.concat([df, row], ignore_index=True)
 
@@ -196,7 +184,6 @@ while (simPos < len(dataFrame)):
         print("appended" + str(appendedTimes))
         appendedTimes = 0
         t0 = millis()
-        err_best = 10000
         
     i += 1
     if(i >= runValues * renderFidelity):
---
+++ HelmholtzDriverV2/runSimulation.py
@@ -0,0 +1,131 @@
+
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+from Dependencies.R4UART import sendPWMValues, readPWMValues, initiateUART, readMagnetometerValues # UART code 
+from Dependencies.calibrateValues import calibrate # magnetometer calibration code 
+from Dependencies.extraneous import processStrings, calculateOffsets, millis # import extraneous functions
+
+
+dataFrame = pd.read_csv("filename") # magnetic fields dataframe
+
+################################################################################ Run parameters
+
+loop = False # if true, simulation will start from the beginning upon reaching the end
+runSpeed = 1 # delay time in seconds between each simulation value, so 1 is real time
+startPosition = 0 # index of the dataframe to start in
+
+################################################################################
+
+#                x  y  z
+currentFields = [0, 0, 0]
+
+#                +x -x +y -y +z -z
+currentPWMVals = [0, 0, 0, 0, 0, 0]
+
+timeVector = []
+timeVar = 0
+
+i = startPosition
+
+terminals = initiateUART()
+time.sleep(1)
+nanoSer = terminals[0]
+R4Ser = terminals[1]
+
+sendPWMValues(0, 0, 0, 0, 0, 0, R4Ser)
+time.sleep(2)
+
+
+magOutputX = []
+magOutputY = []
+magOutputZ = []
+
+simulationX = []
+simulationY = []
+simulationZ = []
+
+while(True):
+
+    ################################################################################################################## magnetometer reading
+    nanoSer.reset_input_buffer()
+    nanoSer.reset_output_buffer()
+    
+    R4Ser.reset_input_buffer()
+    R4Ser.reset_output_buffer()
+    magnetometerOutput = readMagnetometerValues(nanoSer)
+
+    magnetometerOutput = magnetometerOutput.split(" ")
+    #print(magnetometerOutput)
+
+    try:
+        magX = float(magnetometerOutput[0])
+        magY = float(magnetometerOutput[1])
+        magZ = float(magnetometerOutput[2])    
+        
+        calibratedValues = calibrate(magX, magY, magZ) # apply calibration
+
+# #     print("X: " + "{:.2f}".format(magX) + " Y: " + "{:.2f}".format(magY) + " Z: " + "{:.2f}".format(magZ))
+
+        calMagX = round(calibratedValues[0], 2)
+        calMagY = round(calibratedValues[1], 2)
+        calMagZ = round(calibratedValues[2], 2)
+    
+    except:
+        
+        calMagX = magOutputX[len(magOutputX) - 1]
+        calMagY = magOutputY[len(magOutputY) - 1]
+        calMagZ = magOutputZ[len(magOutputZ) - 1]
+    
+# #     # purely for readable format, adds necessary zeros to preserve 2 decimal format
+    magStrings = processStrings(calMagX, calMagY, calMagZ)
+
+    magOutputX.append(calMagX)
+    magOutputY.append(calMagY)
+    magOutputZ.append(calMagZ)
+    print("X: " + str(magStrings[0]) + " Y: " + str(magStrings[1]) + " Z: " + str(magStrings[2]))
+
+
+    currentFields[0] = dataFrame.loc[i, 'Bx']
+    currentFields[1] = dataFrame.loc[i, 'By']
+    currentFields[2] = dataFrame.loc[i, 'Bz']
+
+    simulationX.append(currentFields[0])
+    simulationY.append(currentFields[1])
+    simulationZ.append(currentFields[2])
+
+    currentPWMVals[0] = dataFrame.loc[i, 'Px']
+    currentPWMVals[1] = dataFrame.loc[i, 'Nx']
+    currentPWMVals[2] = dataFrame.loc[i, 'Py']
+    currentPWMVals[3] = dataFrame.loc[i, 'Ny']
+    currentPWMVals[4] = dataFrame.loc[i, 'Pz']
+    currentPWMVals[5] = dataFrame.loc[i, 'Nz']
+
+    sendPWMValues(currentPWMVals[2], currentPWMVals[3], currentPWMVals[1], currentPWMVals[0], currentPWMVals[4], currentPWMVals[5])
+
+    i += 1
+
+    if(i == len(dataFrame) - 1):
+        if(loop):
+            i = startPosition
+        else:
+            break
+
+    timeVector.append(timeVar)
+    timeVar += 1
+
+    time.sleep(runSpeed)
+
+
+fig, ax = plt.subplots(3)
+
+ax[0].plot(timeVector,magOutputX, color = "red", label = "Real")
+ax[0].plot(timeVector, simulationX, color = "blue", label = "PySOL")
+
+ax[1].plot(timeVector,magOutputY, color = "red")
+ax[1].plot(timeVector, simulationY,  color = "blue")
+
+ax[2].plot(timeVector,magOutputZ, color = "red")
+ax[2].plot(timeVector, simulationZ, color = "blue")
