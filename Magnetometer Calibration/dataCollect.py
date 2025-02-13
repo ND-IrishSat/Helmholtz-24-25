c030b76a7b384eeb03689105e43c65116847bfab
thanson47 <thanson042@gmail.com>
2025-02-13 17:23:05 -0500

    way faster magnetometere

--- Magnetometer Calibration/dataCollect.py
+++ Magnetometer Calibration/dataCollect.py
@@ -7,20 +7,21 @@
 import serial
 import time
 
-# tylers mac '/dev/cu.usbmodem2101'
-
-serialPort = '/dev/cu.usbmodem101' # replace with your own usb port 
+# Replace with your own USB port
+serialPort = '/dev/cu.usbmodem101'  
 
 ser = serial.Serial(serialPort, 9600, timeout=1)
 
-with open("arduino_output.txt", "w") as file:
-    while True:
+start_time = time.time()  # Record the start time
+
+with open("arduino_output2.txt", "w") as file:
+    while time.time() - start_time < 10:  # Run for 10 seconds
         line = ser.readline().decode('utf-8').strip()
         if line:
             print(line)
-
             file.write(line + "\n")  # Save the output to the text file
             file.flush()  # Ensure data is written to the file
 
-
+print("Data collection complete. Exiting...")
+ser.close()  # Close the serial connection
  
